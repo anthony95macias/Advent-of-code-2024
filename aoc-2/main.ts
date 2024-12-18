208f4e1e365f6197a75346f4918368694b7cfe94
Anthony Macias <Anthony40macias@gmail.com>
2024-12-17 23:26:45 -0800

    aoc-2: feat: add logic to filter strictly increasing or decreasing reports from input file
    - Implemented functions to check monotonicity (, , ).
    - Processed input from  to parse and filter reports.
    - Output the count of valid reports.added a few lines of more info to the readme file.

--- aoc-2/main.ts
+++ aoc-2/main.ts
@@ -1,9 +1,29 @@
+const increasing = (report: number[]) => {
+  for (let i = 0; i < report.length; i++ ) {
+    if (report[i] > report[i+1])
+      return false
+  }
+    return true;
+};
+
+const decreasing = (report: number[]) => {
+  for (let i = 0; i < report.length; i++ ) {
+    if (report[i] < report[i+1])
+      return false
+  }
+    return true;
+};
+
+const oneDirection = (report: number[]) => {
+  return increasing(report) || decreasing(report);
+};
+
 const text = await Deno.readTextFile("data.txt");
 const lines = text.split("\n").slice(0,-1);
 
 const report = lines.map((line) => {
   const elems = line.split(" ");
   return elems.map((elems) => parseInt(elems)); // Return the mapped array
-})
+}).filter((report) => oneDirection(report)); // Filter the array
 
-console.log(report[report.length - 1]);
+console.log(report.length);
