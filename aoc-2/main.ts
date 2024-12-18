c9ebedd9bd2aa6c7673ffa019bf7566ddc52da04
Anthony Macias <Anthony40macias@gmail.com>
2024-12-18 11:27:05 -0800

    feat: Implement **SOLUTION** for aoc-2
    - Added  and  functions to check if a report is monotonic.
    - Created  function to validate reports as either strictly increasing or decreasing.
    - Parsed data from data.txt into structured numeric arrays.
    - Filtered and sorted valid reports for further processing.
    - Calculated the sum of valid reports based on the difference condition.
    - Logged invalid reports and the final count of valid reports.

--- aoc-2/main.ts
+++ aoc-2/main.ts
@@ -16,14 +16,35 @@ const decreasing = (report: number[]) => {
 
 const oneDirection = (report: number[]) => {
   return increasing(report) || decreasing(report);
-};
-
+}
 const text = await Deno.readTextFile("data.txt");
 const lines = text.split("\n").slice(0,-1);
 
 const report = lines.map((line) => {
   const elems = line.split(" ");
   return elems.map((elems) => parseInt(elems)); // Return the mapped array
-}).filter((report) => oneDirection(report)); // Filter the array
+}).filter((report) => oneDirection(report)) // Filter the array
+.map((report)=> {
+  return report.sort((i,j) => i-j); // Sort the array
+});
 
-console.log(report.length);
+// Print the array of reports that are safe to print out to the console
+report.forEach((report) => {
+  if (!increasing(report) && !decreasing(report)) {
+    console.log(report);
+  }
+});
+
+let sum = 0;
+
+report.forEach((report) => {
+  for (let i = 0; i < report.length -1; i++) {
+    const diff = report[i+1] - report[i];
+    if (diff <= 0 || diff > 3) {
+      return;
+    }
+  }
+  sum += 1;
+});
+
+console.log(sum);
