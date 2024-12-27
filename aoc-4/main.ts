a59d4783109a66036aca9d96ed97e4ed934b2481
Anthony_Macias <118767655+anthony95macias@users.noreply.github.com>
2024-12-27 21:57:42 +0000

    added some more code into main.ts in aoc-4. It as a for loop for the grid and loops over each number to sort the xmas. still a work in progress to finishing the code.

--- aoc-4/main.ts
+++ aoc-4/main.ts
@@ -4,12 +4,20 @@ import * as cowsay from "https://deno.land/x/cowsay@1.1/mod.ts";
 const input = await Deno.readTextFile("data.txt");
 
 // filter the data
-const grid: string[] = input.split("\n").filter(line => !!line)
-console.log(grid[grid.length - 1]);
+const grid: string[] = input.split("\n").filter(line => line.trim() !== "");
+
+let xmases: number = 0;
+
+for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+        console.log(i, j)
+    }
+}
 
 
-// Final answer (output): 
-// const answer = cowsay.say({
-//   text: `The total length of the input is: ${input.length}`,
-// });
-// console.log(answer);
+
+    // Final answer (output): 
+const answer = cowsay.say({
+  text: `The total length of the input is: ${xmases}`,
+});
+console.log(answer);
