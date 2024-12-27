f16549b9f60ad89a6844a1711e6e4bec710ca8c0
Anthony_Macias <118767655+anthony95macias@users.noreply.github.com>
2024-12-27 03:46:14 +0000

    aoc-4 has data read and filtered. needs more work

--- aoc-4/main.ts
+++ aoc-4/main.ts
@@ -1,10 +1,15 @@
 import * as cowsay from "https://deno.land/x/cowsay@1.1/mod.ts";
 
+// read data
 const input = await Deno.readTextFile("data.txt");
 
-// console.log(input)
+// filter the data
+const grid: string[] = input.split("\n").filter(line => !!line)
+console.log(grid[grid.length - 1]);
 
-const answer = cowsay.say({
-  text: `The total length of the input is: ${input.length}`,
-});
-console.log(answer);
+
+// Final answer (output): 
+// const answer = cowsay.say({
+//   text: `The total length of the input is: ${input.length}`,
+// });
+// console.log(answer);
